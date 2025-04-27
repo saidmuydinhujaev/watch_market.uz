91fd00b7f62e8f79992805cdd90d791e21f94248
saidmuydinhujaev <saidhonmujdinhuzaev780@gmail.com>
2025-04-27 15:42:40 +0500

    Fix project structure

--- Entry Django Dev Trip/Entry Django Dev Trip/Entry Django Dev Trip/EntryDjangoDevTrip/project/app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Watch
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Watch
-        fields = '__all__'
